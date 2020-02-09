02294188b1a94acd43420d4fe84982bb073e22bf
pavankhyathi <53396559+pavankhyathi@users.noreply.github.com>
2020-02-09 12:11:58 +0530

    POST_API

---
+++ Post_API.py
@@ -0,0 +1,31 @@
+'''
+Created on 30-Jan-2020
+
+@author: HP
+'''
+import requests
+from speedtest import json
+from jsonpath import jsonpath
+
+
+#POSITIVE TEST CASE
+url= "https://api.postalpincode.in/pincode/523286"
+
+response= requests.get(url)
+json_response= json.loads(response.text)
+
+#print(json_response) 
+name= jsonpath(json_response,'Name')
+
+print(name[0])
+#print(response.content) 
+#print(response.headers)
+
+#NEGATIVE TEST CASE
+#url1= "https://api.postalpincode.in/pincode/56662773"
+
+#response= requests.get(url1)
+#json_response= json.loads(response.text)
+
+#print(json_response) 
+#print(response.headers)
